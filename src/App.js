61f21fd8e2ccbf5f7f4c2fe71fcc869e86a924a6
unknown <pawanrajora290@gmail.com>
2022-12-07 20:37:13 +0530

    app component update

--- src/App.js
+++ src/App.js
@@ -3,23 +3,23 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=''>
+<Header/>
+<LoginButton/>
     </div>
-  );
+  )
 }
 
+function LoginButton(){
+return(
+  <button>Login</button>
+)
+}
+
+function Header(){
+  return(
+    <h1>Header</h1>
+  )
+  }
+  
 export default App;
